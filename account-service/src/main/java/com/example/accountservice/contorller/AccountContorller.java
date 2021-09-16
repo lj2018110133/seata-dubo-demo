d014c27e0a540f3d09f4c3ac9a54b9892e0a2b60
zaifeng <zaifeng.lj@alibaba-inc.com>
2021-09-16 19:32:18 +0800

    add some note

--- account-service/src/main/java/com/example/accountservice/contorller/AccountContorller.java
+++ account-service/src/main/java/com/example/accountservice/contorller/AccountContorller.java
@@ -18,9 +18,11 @@ public class AccountContorller {
 
     //index
     // index12
+
     @RequestMapping(value = "/index")
     @ResponseBody
     public boolean get(int money,String uid) throws SQLException {
+
 
 //        return service.decreaseAccount(money,uid);
         return storageDubboService.decreaseStorage(money,uid);
